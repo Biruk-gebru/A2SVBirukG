07073d94703f05970aeb7e2535bad32fce2aa474
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-05 09:28:27 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 05-Feb-2025/Almost Prime 242090.py
@@ -0,0 +1,18 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+s = 0
+
+for x in range(1, n + 1):
+    sol = set()
+    d = 2
+    while d * d <= x:
+        while x % d == 0:
+            sol.add(d)
+            x //= d
+        d += 1
+    if x > 1:
+        sol.add(x)
+    if len(sol) == 2:
+        s += 1
+print(s)
