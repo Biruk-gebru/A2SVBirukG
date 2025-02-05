a6ac401b2587317c59f6f768588b3473a0634f5a
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-05 15:21:39 +0300

    Created 'Shuffle String' by A2SV Hub

---
+++ 05-Feb-2025/Shuffle String 242960.py
@@ -0,0 +1,13 @@
+# Problem: Shuffle String - https://leetcode.com/problems/shuffle-string/description/
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        dic={}
+        for i in range(len(s)):
+            dic[indices[i]]=s[i]
+        s=sorted(dic.items())
+        print(s)
+        ss=''
+        for i in  range(len(s)):
+            ss+=s[i][1]
+        return ss
