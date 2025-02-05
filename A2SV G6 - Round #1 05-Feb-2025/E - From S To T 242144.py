77625aab65f835009da3c360507a9a0b380d45c4
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-05 09:59:00 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 05-Feb-2025/E - From S To T 242144.py
@@ -0,0 +1,47 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+t=1
+t= int(input())
+for _ in range(t):
+	s = list(input())
+	s.pop()
+	t = list(input())
+	t.pop()
+	p = list(input())
+	p.pop()
+	k = s + p
+	dict_s = Counter(s)
+	dict_t = Counter(t)
+	dict_k = Counter(k)
+	flag = True
+	for elem in s: 
+		if elem in t:
+			if dict_s[elem]> dict_t[elem]:
+				flag = False
+		else:
+			flag = False
+				
+	for elem in t:
+		if elem in k:
+			if dict_k[elem] < dict_t[elem]:
+				flag = False
+		else:
+			flag = False
+	i = 0 
+	for elem in t:
+		if elem == s[i]:
+			i+=1
+			if i >=len(s):
+				break
+
+	if i < len(s):
+		flag = False
+		
+	if flag:
+		print("YES")
+	else:
+		print("NO")
