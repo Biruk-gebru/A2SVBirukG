8b13a5878784a0a80fbb0f835e8e0dc24a95d864
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-06 10:28:44 +0300

    Created 'Runner-up Score' by A2SV Hub

---
+++ 06-Feb-2025/Runner-up Score 245826.py
@@ -0,0 +1,7 @@
+# Problem: Runner-up Score - https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    a = list(set(sorted(arr)))
+    print(a[-2])
