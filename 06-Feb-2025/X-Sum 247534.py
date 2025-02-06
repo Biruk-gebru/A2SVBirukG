24f67f88f4fac117de5de6420d10cfab10f7e4c8
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-06 18:54:47 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 06-Feb-2025/X-Sum 247534.py
@@ -0,0 +1,44 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+t = int(input())  
+
+for _ in range(t):  
+    board = []  
+    m, n = map(int, input().split())  
+    for _ in range(m):  
+        row = list(map(int, input().split()))  
+        board.append(row)  
+    
+    max_sum = float('-inf')  
+    
+    for i in range(m):  
+        for k in range(n):  
+            temp = board[i][k]  
+            
+            j, iin = k - 1, i - 1  
+            while iin >= 0 and j >= 0:  
+                temp += board[iin][j]  
+                iin -= 1  
+                j -= 1  
+            
+            j, iin = k + 1, i + 1  
+            while iin < m and j < n:   
+                temp += board[iin][j]  
+                iin += 1  
+                j += 1  
+            
+            j, iin = k - 1, i + 1  
+            while iin < m and j >= 0:  
+                temp += board[iin][j]  
+                iin += 1  
+                j -= 1  
+            
+            j, iin = k + 1, i - 1  
+            while iin >= 0 and j < n:  
+                temp += board[iin][j]  
+                iin -= 1  
+                j += 1  
+            
+            max_sum = max(max_sum, temp)  
+    
+    print(max_sum)
