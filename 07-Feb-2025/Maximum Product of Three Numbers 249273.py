c64476369d1b29c51a104fe179945981cf6a195e
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-07 11:24:32 +0300

    Created 'Maximum Product of Three Numbers' by A2SV Hub

---
+++ 07-Feb-2025/Maximum Product of Three Numbers 249273.py
@@ -0,0 +1,27 @@
+# Problem: Maximum Product of Three Numbers - https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+
+from typing import List  
+
+class Solution:  
+    def maximumProduct(self, nums: List[int]) -> int:   
+        
+        pos = [p for p in nums if p >= 0]  
+        neg = [n for n in nums if n < 0]  
+        
+        pos.sort(reverse=True)  
+        neg.sort()  
+
+        max_product = float('-inf')  
+
+        
+        if len(pos) >= 3:  
+            max_product = max(max_product, pos[0] * pos[1] * pos[2])  
+
+        if len(neg) >= 2 and len(pos) > 0:  
+            max_product = max(max_product, neg[0] * neg[1] * pos[0]) 
+
+        if len(neg) >= 3 and len(pos) == 0:
+             max_product = max(max_product, neg[-3] * neg[-2] * neg[-1])
+  
+
+        return max_product
