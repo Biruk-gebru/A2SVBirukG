efc9d13790e006879c322dc32cffe15cd37978b6
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-12 12:00:38 +0300

    Created 'Helpful Maths' by A2SV Hub

---
+++ 12-Feb-2025/Helpful Maths 259215.py
@@ -0,0 +1,16 @@
+# Problem: Helpful Maths - https://codeforces.com/problemset/problem/339/A
+
+op = input()
+op = list(op)
+newA = []
+for n in op:
+    if n == "+":
+        continue 
+    newA.append(n)
+newA.sort()
+sol = []
+for i in range(len(newA)):
+    sol.append(newA[i])
+    sol.append("+")
+sol.pop()
+print("".join(sol))
