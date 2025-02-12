50dc6aa05266815409d4e37ba81c200c18db5e73
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-12 11:58:10 +0300

    Created 'Young Physicist' by A2SV Hub

---
+++ 12-Feb-2025/Young Physicist 259208.py
@@ -0,0 +1,18 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+t = int(input())
+x = []
+y = []
+z = []
+for _ in range(t):
+    num = list(map(int, input().split()))
+    x.append(num[0])
+    y.append(num[1])
+    z.append(num[2])
+xSum = sum(x)
+ySum = sum(y)
+zSum = sum(z)
+if xSum!= 0 or ySum != 0 or zSum != 0:
+    print("NO")
+else:
+    print("YES")
