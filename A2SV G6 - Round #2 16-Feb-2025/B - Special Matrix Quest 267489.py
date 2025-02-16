63b9c38e57ea39024fa40c630dcb8c37826930ed
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-16 09:40:37 +0300

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 16-Feb-2025/B - Special Matrix Quest 267489.py
@@ -0,0 +1,30 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+t = int(input())
+matrix = []
+for _ in range(t):
+    n = list(map(int, input().split()))
+    matrix.append(n)
+
+if t == 1:
+    print(matrix[0][0])
+else:
+    idxCi = (t - 1)//2
+    idxCj = (t-1)//2
+    sumDM = 0
+    sumCC = 0
+    sumCR = 0
+    sumDS = 0
+    for i in range(t):
+        for j in range(t):
+            if i == j:
+                sumDM += matrix[i][j]
+            if j == (t -1)/2:
+                sumCC += matrix[i][j]
+            if i == (t - 1)/2:
+                sumCR += matrix[i][j]
+            if i + j == t - 1:
+                sumDS += matrix[i][j]
+    # print(sumCC , sumCR , sumDM , sumDS , sumC)
+    print(sumCC + sumCR + sumDM + sumDS - (matrix[idxCi][idxCj])*3)
+    
