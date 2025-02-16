1f1966fbf6a3b4b4c15b5f202c3f7c3f2f44e48e
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-16 09:40:25 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 16-Feb-2025/A - Make the Product Zero 267488.py
@@ -0,0 +1,13 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+t=1
+#t= int(input())
+for _ in range(t):
+	len = int(input())
+	nums = list(map(int, input().split()))
+	minA = float('inf')
+	
+	for elem in nums:
+		if abs(elem) < minA:
+			minA = abs(elem)
+	print(abs(minA))
