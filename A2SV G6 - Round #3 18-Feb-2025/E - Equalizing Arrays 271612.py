096892881991d25ecc33800b181ffe9c655c5311
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-18 15:10:00 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/E - Equalizing Arrays 271612.py
@@ -0,0 +1,28 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())  
+a = list(map(int, input().split()))  
+m = int(input())  
+b = list(map(int, input().split()))  
+
+if sum(a) != sum(b):
+    print(-1)
+else:
+    sa = [a[0]]
+    for i in range(1,n):
+        sa.append(sa[-1] + a[i])
+    sb = [b[0]]
+    for i in range(1,m):
+        sb.append(sb[-1] + b[i])
+    ida = idb = 0
+    sol = 0
+    while ida < n and idb < m:
+        if sa[ida] == sb[idb]:
+            sol += 1
+            ida += 1
+            idb += 1
+        elif sa[ida] < sb[idb]:
+            ida += 1
+        else:
+            idb += 1
+    print(sol)
