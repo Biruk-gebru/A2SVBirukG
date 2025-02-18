bc160a66132b8ffcc96ad5754a1474c062b06020
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-18 15:10:32 +0300

    Created 'B - Anansi and Trip-Photographs' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/B - Anansi and Trip-Photographs 271616.py
@@ -0,0 +1,21 @@
+# Problem: B - Anansi and Trip-Photographs - https://codeforces.com/gym/588468/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n , x = map(int, input().split())
+    row = sorted(list(map(int, input().split())))
+
+    flag = True
+    for i in range(n):
+        j = i + n
+        if abs(row[i] - row[j]) >= x:
+            j -= 1
+        else:
+            flag = False
+            break
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+
