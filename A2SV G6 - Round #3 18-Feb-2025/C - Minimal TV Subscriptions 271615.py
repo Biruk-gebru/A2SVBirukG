abf606dae83c0f88a41a8ed2c908d87554bc2a5e
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-18 15:10:19 +0300

    Created 'C - Minimal TV Subscriptions' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/C - Minimal TV Subscriptions 271615.py
@@ -0,0 +1,21 @@
+# Problem: C - Minimal TV Subscriptions - https://codeforces.com/gym/588468/problem/C
+
+from collections import defaultdict
+t = int(input())
+
+for _ in range(t):
+    n,k,d = map(int, input().split())
+    a = list(map(int, input().split()))
+    holder = defaultdict(int)
+    for i in range(d):
+        holder[a[i]] += 1
+    sol = len(holder)
+    left = 0
+    for right in range(d,n):
+        holder[a[left]] -= 1
+        if not holder[a[left]]:
+            del holder[a[left]]
+        holder[a[right]] += 1
+        left += 1
+        sol = min(sol,len(holder))
+    print(sol)
