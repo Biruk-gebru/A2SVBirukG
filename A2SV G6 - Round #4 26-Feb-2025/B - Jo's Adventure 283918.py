3d54a6e33e12ef863d06734f56dc869049e4154d
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-26 17:49:41 +0300

    Created 'B - Jo's Adventure' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/B - Jo's Adventure 283918.py
@@ -0,0 +1,41 @@
+# Problem: B - Jo's Adventure - https://codeforces.com/gym/590053/problem/B
+
+n , t = map(int,input().split())
+
+arr = list(map(int,input().split()))
+first_last = [0]
+last_first = [0]
+for i in range(n-1):
+    if arr[i] > arr[i+1]:
+        first_last.append(arr[i] - arr[i+ 1])
+    else:
+        first_last.append(0)
+for i in range(n-1,0,-1):
+    if arr[i] > arr[i-1]:
+        last_first.append(arr[i] - arr[i - 1])
+    else:
+        last_first.append(0)
+last_first.append(0)
+last_first.reverse()
+prevfl = []
+su = 0 
+for i in range(n):
+    prevfl.append(first_last[i] + su)
+    su += first_last[i]
+prevlf = []
+su = 0 
+for i in range(n):
+    prevlf.append(last_first[i] + su)
+    su += last_first[i]
+# prevlf.reverse()
+for _ in range(t):
+    l, r = map(int, input().split())
+    if l < r:
+        print(prevfl[r-1] - prevfl[l-1])
+    else:
+        print(prevlf[l-1]- prevlf[r-1])
+
+
+
+    
+
