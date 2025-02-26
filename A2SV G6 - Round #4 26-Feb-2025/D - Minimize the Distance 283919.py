c99413427617b655366ce4c5d724b6c168a486cf
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-02-26 17:49:46 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 26-Feb-2025/D - Minimize the Distance 283919.py
@@ -0,0 +1,7 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+t = int(input())
+arr = list(map(int, input().split()))
+su = 0
+arr.sort()
+print(arr[((t + 1) //2) - 1])
