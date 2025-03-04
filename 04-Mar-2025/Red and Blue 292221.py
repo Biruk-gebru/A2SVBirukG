36669bd69c5953fe354175f2eb4bbc0e4bcc2af8
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-03-04 22:06:03 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 04-Mar-2025/Red and Blue 292221.py
@@ -0,0 +1,15 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    for i in range(1,n):
+        a[i] = a[i-1] + a[i]
+    m = int(input())
+    b = list(map(int, input().split()))
+    for i in range(1,m):
+        b[i] = b[i-1] + b[i]
+    print(max(0, max(a), max(b), max(a) + max(b)))
+    
