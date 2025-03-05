b846533cb676c3731f77069acea517f3c3f9a1fc
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-03-05 17:24:44 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 05-Mar-2025/Array Splitting 292987.py
@@ -0,0 +1,14 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+n , k = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+diff = []
+for i in range(n-1):
+    diff.append(a[i+1] - a[i])
+
+nu = len(diff) - k + 1
+diff.sort()
+print(sum(diff[:nu]))
+
