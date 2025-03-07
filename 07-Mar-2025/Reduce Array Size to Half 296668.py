e1513c32b0e3cb35f94db0e99063587f82a133ea
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-03-07 17:26:01 +0300

    Created 'Reduce Array Size to Half' by A2SV Hub

---
+++ 07-Mar-2025/Reduce Array Size to Half 296668.py
@@ -0,0 +1,14 @@
+# Problem: Reduce Array Size to Half - https://leetcode.com/problems/reduce-array-size-to-the-half/
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        c = Counter(arr)
+        c = sorted(c.items(),key = lambda x: x[1], reverse = True)
+        sol = 0
+        curr = len(arr)
+        for k in c:
+            sol += 1
+            curr -= k[1]
+            if curr <= len(arr)//2:
+                break
+        return sol
