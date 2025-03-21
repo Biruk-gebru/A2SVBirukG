592e3aacdcecf5caa5c5e98907bac55b3b244d98
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-03-21 17:54:33 +0300

    Created 'Balance a Binary Search Tree' by A2SV Hub

---
+++ 21-Mar-2025/Balance a Binary Search Tree 314599.py
@@ -0,0 +1,33 @@
+# Problem: Balance a Binary Search Tree - https://leetcode.com/problems/balance-a-binary-search-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        nums = []
+        def db(root):
+            if root:
+                nums.append(root.val)
+            else:
+                return
+            db(root.left)
+            db(root.right)
+        db(root)
+        def dfs(left,right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+
+            node.left = dfs(left, mid - 1)
+            node.right = dfs(mid + 1, right)
+
+            return node
+        nums.sort()
+        n = len(nums)
+        return dfs(0 , n - 1)
+        
