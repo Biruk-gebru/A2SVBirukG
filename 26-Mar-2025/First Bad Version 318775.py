324f717b2739cb6143fa7b8f0c477c91e054d145
Biruk-gebru <72378440+Biruk-gebru@users.noreply.github.com>
2025-03-26 17:25:47 +0300

    Created 'First Bad Version' by A2SV Hub

---
+++ 26-Mar-2025/First Bad Version 318775.py
@@ -0,0 +1,12 @@
+# Problem: First Bad Version - https://leetcode.com/problems/first-bad-version/
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left,right=1,n
+        while left<right:
+            mid=left+(right-left)//2
+            if isBadVersion(mid):
+                right=mid
+            else:
+                left=mid+1
+        return left
